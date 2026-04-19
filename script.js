4d88252e694a6dfb89e8843803270dc844fe56b2
Kenneth Kobina Essien Aidoo <essienkent@gmail.com>
2026-04-19 22:22:38 +0000

    to be continued

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+
+const options = [
+    {id:"option1",text:"Javascript",votes: 0},
+    {id:"option2",text:"Pythoon",votes: 0},
+    {id:"option3",text:"Java",votes: 0},
+    {id:"option4",text:"C++",votes: 0}
+]
 function submitVote(){
 
     const selectedOption = document.querySelector('input[name="poll"]:checked');
 
-    if(selectedOption){
+    if(!selectedOption){
         alert("Please select an option");
         return;
     }
+
+
 }
